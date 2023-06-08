41b00375a559eaeaedb1c4b04ca2b04133ec2b54
namth1 <namth1@vuihoc.vn>
2023-06-08 15:22:31 +0700

    random

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-function GameScreen() {
+import NumberContainer from "../components/game/NumberContainer";
+import Title from "../components/ui/Title";
+
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
   return (
     <View style={styles.screen}>
-      <Text>Opponent's Guess</Text>
-      {/* GUESS */}
+      <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
         {/* + - */}
@@ -19,6 +36,6 @@ export default GameScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    padding: 0,
+    padding: 24,
   },
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 
-import PrimaryButton from "../components/PrimaryButton";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
