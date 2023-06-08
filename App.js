c55645b3b398add5b6ee3e0977879f3512817d6f
namth1 <namth1@vuihoc.vn>
2023-06-08 14:49:20 +0700

    safe area

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
@@ -26,7 +27,7 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
