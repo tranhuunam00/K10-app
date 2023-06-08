fe48cbd803f38b0053dbb8374e8d5fdf4d5554c3
namth1 <namth1@vuihoc.vn>
2023-06-08 11:26:31 +0700

    style

--- App.js
+++ App.js
@@ -9,49 +9,11 @@ import {
   TextInput,
   View,
 } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   const [item, setItem] = useState("");
   const [listItems, setListItems] = useState([]);
 
-  return (
-    <View style={styles.container}>
-      <View style={{ marginTop: 100 }}>
-        <TextInput
-          style={{ marginBottom: 10, width: 100, height: 70 }}
-          onChangeText={(e) => {
-            setItem(e);
-          }}
-          placeholder="item.."
-        ></TextInput>
-        <Button
-          onPress={() => {
-            setListItems([...listItems, item]);
-          }}
-          title="Add "
-        ></Button>
-      </View>
-
-      <FlatList
-        data={listItems}
-        keyExtractor={(value) => {
-          return value + new Date().getTime() + Math.random(100);
-        }}
-        renderItem={(item) => (
-          <View style={{ height: 100, width: 100, backgroundColor: "#ff00ff" }}>
-            <Text>{item.item}</Text>
-          </View>
-        )}
-      ></FlatList>
-    </View>
-  );
+  return <StartGameScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
