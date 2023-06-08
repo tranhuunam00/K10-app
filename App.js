0bf77a44888f10aabcd5ef86371fda18d48160c1
namth1 <namth1@vuihoc.vn>
2023-06-08 11:54:02 +0700

    change background

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import {
-  Button,
-  FlatList,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import { StyleSheet, View } from "react-native";
+
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  const [item, setItem] = useState("");
-  const [listItems, setListItems] = useState([]);
-
-  return <StartGameScreen />;
+  return (
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: "#ddb52f",
+  },
+});
