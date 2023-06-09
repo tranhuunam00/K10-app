96ced4fc2b681a526ad5b210f10bc610193cfcc7
namth1 <namth1@vuihoc.vn>
2023-06-09 10:56:53 +0700

    platform

--- components/ui/Title.ios.js
+++ components/ui/Title.ios.js
@@ -1,21 +1,22 @@
-import { Text, StyleSheet } from "react-native";
-
-import Colors from "../../constants/colors";
+import { Text, StyleSheet, Platform } from "react-native";
 
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
 }
+
 export default Title;
 
 const styles = StyleSheet.create({
   title: {
     fontFamily: "open-sans-bold",
     fontSize: 24,
-    // fontWeight: "bold",
-    color: Colors.accent500,
+    // fontWeight: 'bold',
+    color: "white",
     textAlign: "center",
-    borderWidth: 2,
-    borderColor: Colors.accent500,
+    // borderWidth: Platform.OS === 'android' ? 2 : 0,
+    // borderWidth: Platform.select({ ios: 0, android: 2 }),
     padding: 12,
+    maxWidth: "80%",
+    width: 300,
   },
 });
