025bb85debe5990a34e7f8825c712738d2fcd324
namth1 <namth1@vuihoc.vn>
2023-06-09 09:47:26 +0700

    dimension

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,6 +1,6 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Dimensions } from "react-native";
 
-import Colors from '../../constants/colors';
+import Colors from "../../constants/colors";
 
 function Card({ children }) {
   return <View style={styles.card}>{children}</View>;
@@ -8,17 +8,19 @@ function Card({ children }) {
 
 export default Card;
 
+const deviceWidth = Dimensions.get("window").width;
+
 const styles = StyleSheet.create({
   card: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 36,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: deviceWidth < 380 ? 18 : 36,
     marginHorizontal: 24,
     padding: 16,
     backgroundColor: Colors.primary800,
     borderRadius: 8,
     elevation: 4,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
