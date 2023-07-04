19ca7c333f5378a3c938aade2acad0ebf8bf9207
namTh <tranhuunam23022000@gmail.com>
2023-07-04 23:33:55 +0700

    permision camera

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -6,13 +6,40 @@ import {
 } from 'expo-image-picker';
 
 function ImagePicker() {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      const permissionResponse = await requestPermission();
+      Alert.alert(
+        'Insufficient Permissions!',
+        'You need to grant camera permissions to use this app.'
+      );
+      return false;
+    }
+
+    return true;    
+  }
+
   async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log("ĐÃ có IMAGE");
     console.log(image);
   }
 
