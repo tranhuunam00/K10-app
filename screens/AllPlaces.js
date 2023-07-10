9577a32da2b26bdd065740174d40c671891c05c7
namth1 <namth1@vuihoc.vn>
2023-07-10 15:10:57 +0700

    get data and show in list all place

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,7 +1,20 @@
-import PlacesList from '../components/Places/PlacesList';
+import { useEffect, useState } from "react";
+import { useIsFocused } from "@react-navigation/native";
 
-function AllPlaces() {
-  return <PlacesList />;
+import PlacesList from "../components/Places/PlacesList";
+
+function AllPlaces({ route }) {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    }
+  }, [isFocused, route]);
+
+  return <PlacesList places={loadedPlaces} />;
 }
 
 export default AllPlaces;
