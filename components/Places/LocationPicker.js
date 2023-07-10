527520e9955b07600cfde0e284ed563136b3c825
namth1 <namth1@vuihoc.vn>
2023-07-10 15:04:22 +0700

    convert location to address

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -13,7 +13,7 @@ import {
 
 import { Colors } from "../../constants/colors";
 import OutlinedButton from "../UI/OutlinedButton";
-import { getMapPreview } from "../../util/location";
+import { getAddress, getMapPreview } from "../../util/location";
 
 function LocationPicker({ onPickLocation }) {
   const [pickedLocation, setPickedLocation] = useState();
@@ -38,7 +38,11 @@ function LocationPicker({ onPickLocation }) {
   useEffect(() => {
     async function handleLocation() {
       if (pickedLocation) {
-        onPickLocation({ ...pickedLocation });
+        const address = await getAddress(
+          pickedLocation.lat,
+          pickedLocation.lng
+        );
+        onPickLocation({ ...pickedLocation, address: address });
       }
     }
 
