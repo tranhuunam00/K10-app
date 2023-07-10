7ae8df8cdcb9a973f1c11744d1e85f8b144c68e9
namth1 <namth1@vuihoc.vn>
2023-07-10 12:07:26 +0700

    interactive by map

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import AllPlaces from './screens/AllPlaces';
-import AddPlace from './screens/AddPlace';
-import IconButton from './components/UI/IconButton';
-import { Colors } from './constants/colors';
+import AllPlaces from "./screens/AllPlaces";
+import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/UI/IconButton";
+import { Colors } from "./constants/colors";
+import Map from "./screens/Map";
 
 const Stack = createNativeStackNavigator();
 
@@ -25,13 +26,13 @@ export default function App() {
             name="AllPlaces"
             component={AllPlaces}
             options={({ navigation }) => ({
-              title: 'Your Favorite Places',
+              title: "Your Favorite Places",
               headerRight: ({ tintColor }) => (
                 <IconButton
                   icon="add"
                   size={24}
                   color={tintColor}
-                  onPress={() => navigation.navigate('AddPlace')}
+                  onPress={() => navigation.navigate("AddPlace")}
                 />
               ),
             })}
@@ -40,9 +41,10 @@ export default function App() {
             name="AddPlace"
             component={AddPlace}
             options={{
-              title: 'Add a new Place',
+              title: "Add a new Place",
             }}
           />
+          <Stack.Screen name="Map" component={Map} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
